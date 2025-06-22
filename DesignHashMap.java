eeb799f4689e78e78508b6351b1dd70c5d5acf6a
Ishan Tharwal <ishan.tharwal@educationatwork.org>
2025-06-22 02:03:16 -0700

    Done Design2

---
+++ DesignHashMap.java
@@ -0,0 +1,84 @@
+// Time Complexity :  O(l) - l is the length of the LinkedList
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Had to revise basic syntax
+
+
+// Your code here along with comments explaining your approach
+
+class MyHashMap {
+
+    class Node{
+        int key, value;
+        Node next;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }        
+    }
+
+    Node find(Node dummy, int key){
+        Node prev = dummy;
+        Node curr = dummy.next;
+        while (curr != null && curr.key != key){
+            prev = curr;
+            curr = curr.next;
+        }
+        return prev;
+    }
+
+    int buckets;
+    Node[] storage;
+    int getBuckets(int key){
+        return key % buckets;
+    }
+    public MyHashMap() {
+        buckets = 10000;
+        storage = new Node[this.buckets];
+    }
+    
+    public void put(int key, int value) {
+        int bucket = getBuckets(key);
+        if (storage[bucket] == null){
+            storage[bucket] = new Node(-1, -1);
+        }
+        Node prev = find(storage[bucket], key);
+        if(prev.next == null) {
+            prev.next = new Node(key, value);
+        } else{
+            prev.next.value = value;
+        }
+    }
+    
+    public int get(int key) {
+        int bucket = getBuckets(key);
+        if (storage[bucket] == null){
+            return -1;
+        }
+        Node prev = find(storage[bucket], key);
+        if(prev.next != null) {
+            return prev.next.value;
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int bucket = getBuckets(key);
+        if (storage[bucket] == null){
+            return;
+        }
+        Node prev = find(storage[bucket], key);
+        if(prev.next != null) {
+            prev.next = prev.next.next;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
